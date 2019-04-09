c7e0659c99518c52612f485eb21bb11052b82996
Vahdet Unal <vahdetunal58@gmail.com>
2019-04-09 19:04:20 +0300

    refactor: Modify pupular_articles to use execute_query

--- log_analysis.py
+++ log_analysis.py
@@ -19,18 +19,12 @@ def execute_query(query_string):
 
 def popular_articles():
     # Returns the three most viewed articles from news database
-    conn = psycopg2.connect("dbname='news'")
-    cur = conn.cursor()
-    cur.execute(
-        "select articles.title, count(*) as num "
-        "from articles, log "
-        "where log.path like '/article/' || articles.slug "
-        "and log.status='200 OK' and log.method='GET' "
-        "group by title "
-        "order by num desc "
-        "limit 3;"
-    )
-    result = cur.fetchall()
-    cur.close()
-    conn.close()
-    return result
+    query = ("select articles.title, count(*) as num "
+             "from articles, log "
+             "where log.path like '/article/' || articles.slug "
+             "and log.status='200 OK' and log.method='GET' "
+             "group by title "
+             "order by num desc "
+             "limit 3;"
+             )
+    return execute_query(query)
