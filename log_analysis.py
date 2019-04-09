779db070edf05e4d2cb399836e977aba8e027ca0
Vahdet Unal <vahdetunal58@gmail.com>
2019-04-09 18:56:13 +0300

    feat: Add execute_query function

--- log_analysis.py
+++ log_analysis.py
@@ -2,6 +2,21 @@
 import psycopg2
 
 
+def execute_query(query_string):
+    """Executes the given query.
+
+    Arguments:
+    query_string -- The SQL query to be executed
+    """
+    conn = psycopg2.connect("dbname='news'")
+    cur = conn.cursor()
+    cur.execute(query_string)
+    result = cur.fetchall()
+    cur.close()
+    conn.close()
+    return result
+
+
 def popular_articles():
     # Returns the three most viewed articles from news database
     conn = psycopg2.connect("dbname='news'")
